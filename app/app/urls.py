21ee5ef7bd952e58010399e7ce9aa4506599d2f9
Chris Weinell <weinellcj@live.com>
2023-03-30 23:41:45 -0400

    revert STATIC_URL pathing

--- app/app/urls.py
+++ app/app/urls.py
@@ -3,8 +3,11 @@ Project urls.
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from django.views.generic import RedirectView
+
+from . import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
