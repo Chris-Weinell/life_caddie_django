26446fb7dc6f91dead526e0978b99945d9f8c72b
Chris Weinell <weinellcj@live.com>
2023-04-01 11:55:22 -0400

    loading .pem files as .env vars

--- proxy/run.sh
+++ proxy/run.sh
@@ -3,4 +3,6 @@
 set -e
 
 envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
+envsubst < /etc/letsencrypt/live/yourlifecaddie.com/fullchain.pem.tpl > /etc/letsencrypt/live/yourlifecaddie.com/fullchain.pem
+envsubst < /etc/letsencrypt/live/yourlifecaddie.com/privkey.pem.tpl > /etc/letsencrypt/live/yourlifecaddie.com/privkey.pem
 nginx -g 'daemon off;'
