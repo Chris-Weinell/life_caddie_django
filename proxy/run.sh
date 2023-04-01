0cd4fd5c13e111382b2615baa6bb7edc6880ac62
Chris Weinell <weinellcj@live.com>
2023-04-01 12:13:48 -0400

    add cat commands for testing

--- proxy/run.sh
+++ proxy/run.sh
@@ -3,6 +3,8 @@
 set -e
 
 envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
+cat /etc/letsencrypt/live/yourlifecaddie.com/fullchain.pem.tpl
 envsubst < /etc/letsencrypt/live/yourlifecaddie.com/fullchain.pem.tpl > /etc/letsencrypt/live/yourlifecaddie.com/fullchain.pem
+cat /etc/letsencrypt/live/yourlifecaddie.com/fullchain.pem
 envsubst < /etc/letsencrypt/live/yourlifecaddie.com/privkey.pem.tpl > /etc/letsencrypt/live/yourlifecaddie.com/privkey.pem
 nginx -g 'daemon off;'
