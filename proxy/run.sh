5717521433068494166831f6c07b2a0d5a6d4853
Chris Weinell <weinellcj@live.com>
2023-04-01 10:59:43 -0400

    revert changes

--- proxy/run.sh
+++ proxy/run.sh
@@ -3,6 +3,4 @@
 set -e
 
 envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
-envsubst < /etc/nginx/SSL_CERT.pem > /etc/letsencrypt/live/yourlifecaddie.com/fullchain.pem
-envsubst < /etc/nginx/SSL_CERT_KEY.pem > /etc/letsencrypt/live/yourlifecaddie.com/privkey.pem
 nginx -g 'daemon off;'
