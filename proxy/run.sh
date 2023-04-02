8b2d60520d975e9b8f6a1aa856749ea8e29dae10
Chris Weinell <weinellcj@live.com>
2023-04-02 12:12:51 -0400

    reorder dockerfile

--- proxy/run.sh
+++ proxy/run.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
 envsubst < /etc/letsencrypt/live/yourlifecaddie.com/fullchain.pem.tpl > /etc/letsencrypt/live/yourlifecaddie.com/fullchain.pem
 envsubst < /etc/letsencrypt/live/yourlifecaddie.com/privkey.pem.tpl > /etc/letsencrypt/live/yourlifecaddie.com/privkey.pem
 envsubst < /etc/letsencrypt/live/yourlifecaddie.com/cert.pem.tpl > /etc/letsencrypt/live/yourlifecaddie.com/cert.pem
 envsubst < /etc/letsencrypt/live/yourlifecaddie.com/chain.pem.tpl > /etc/letsencrypt/live/yourlifecaddie.com/chain.pem
+envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
 nginx -g 'daemon off;'
