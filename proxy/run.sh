2feb88bb28864eae44a87630e47b20e0256dd056
Chris Weinell <weinellcj@live.com>
2023-04-02 22:57:53 -0400

    troubleshoot

--- proxy/run.sh
+++ proxy/run.sh
@@ -2,8 +2,7 @@
 
 set -e
 
-cat /etc/nginx/default.conf.tpl
-cat /home/life_caddie_django/app/proxy/fullchain.pem.tpl
+cat /etc/letsencrypt/live/yourlifecaddie.com/fullchain.pem.tpl > /etc/letsencrypt/live/yourlifecaddie.com/fullchain.pem
 cat /etc/letsencrypt/live/yourlifecaddie.com/privkey.pem.tpl > /etc/letsencrypt/live/yourlifecaddie.com/privkey.pem
 cat /etc/letsencrypt/live/yourlifecaddie.com/cert.pem.tpl > /etc/letsencrypt/live/yourlifecaddie.com/cert.pem
 cat /etc/letsencrypt/live/yourlifecaddie.com/chain.pem.tpl > /etc/letsencrypt/live/yourlifecaddie.com/chain.pem
