3fd0ce11bfaa8412cd3fbe5c3af6de7a8a5e73cc
Chris Weinell <weinellcj@live.com>
2023-04-02 21:38:14 -0400

    update file path in run.sh

--- proxy/run.sh
+++ proxy/run.sh
@@ -6,9 +6,9 @@ set -e
 # envsubst < /etc/letsencrypt/live/yourlifecaddie.com/privkey.pem.tpl > /etc/letsencrypt/live/yourlifecaddie.com/privkey.pem
 # envsubst < /etc/letsencrypt/live/yourlifecaddie.com/cert.pem.tpl > /etc/letsencrypt/live/yourlifecaddie.com/cert.pem
 # envsubst < /etc/letsencrypt/live/yourlifecaddie.com/chain.pem.tpl > /etc/letsencrypt/live/yourlifecaddie.com/chain.pem
-cat fullchain.pem.tpl > /etc/letsencrypt/live/yourlifecaddie.com/fullchain.pem
-cat privkey.pem.tpl > /etc/letsencrypt/live/yourlifecaddie.com/privkey.pem
-cat cert.pem.tpl > /etc/letsencrypt/live/yourlifecaddie.com/cert.pem
-cat chain.pem.tpl > /etc/letsencrypt/live/yourlifecaddie.com/chain.pem
+cat /etc/letsencrypt/live/yourlifecaddie.com/fullchain.pem.tpl > /etc/letsencrypt/live/yourlifecaddie.com/fullchain.pem
+cat /etc/letsencrypt/live/yourlifecaddie.com/privkey.pem.tpl > /etc/letsencrypt/live/yourlifecaddie.com/privkey.pem
+cat /etc/letsencrypt/live/yourlifecaddie.com/cert.pem.tpl > /etc/letsencrypt/live/yourlifecaddie.com/cert.pem
+cat /etc/letsencrypt/live/yourlifecaddie.com/chain.pem.tpl > /etc/letsencrypt/live/yourlifecaddie.com/chain.pem
 envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
 nginx -g 'daemon off;'
