a0b69cc9b24d3e7a2c19be03e9b8795842dabbd2
Chris Weinell <weinellcj@live.com>
2023-04-02 20:57:24 -0400

    remove > output redirect in run.sh

--- proxy/run.sh
+++ proxy/run.sh
@@ -2,9 +2,13 @@
 
 set -e
 
-envsubst < /etc/letsencrypt/live/yourlifecaddie.com/fullchain.pem.tpl | tee /etc/letsencrypt/live/yourlifecaddie.com/fullchain.pem
-envsubst < /etc/letsencrypt/live/yourlifecaddie.com/privkey.pem.tpl | tee /etc/letsencrypt/live/yourlifecaddie.com/privkey.pem
-envsubst < /etc/letsencrypt/live/yourlifecaddie.com/cert.pem.tpl | tee /etc/letsencrypt/live/yourlifecaddie.com/cert.pem
-envsubst < /etc/letsencrypt/live/yourlifecaddie.com/chain.pem.tpl | tee /etc/letsencrypt/live/yourlifecaddie.com/chain.pem
+# envsubst < /etc/letsencrypt/live/yourlifecaddie.com/fullchain.pem.tpl > /etc/letsencrypt/live/yourlifecaddie.com/fullchain.pem
+# envsubst < /etc/letsencrypt/live/yourlifecaddie.com/privkey.pem.tpl > /etc/letsencrypt/live/yourlifecaddie.com/privkey.pem
+# envsubst < /etc/letsencrypt/live/yourlifecaddie.com/cert.pem.tpl > /etc/letsencrypt/live/yourlifecaddie.com/cert.pem
+# envsubst < /etc/letsencrypt/live/yourlifecaddie.com/chain.pem.tpl > /etc/letsencrypt/live/yourlifecaddie.com/chain.pem
+envsubst < /etc/letsencrypt/live/yourlifecaddie.com/fullchain.pem.tpl
+envsubst < /etc/letsencrypt/live/yourlifecaddie.com/privkey.pem.tpl
+envsubst < /etc/letsencrypt/live/yourlifecaddie.com/cert.pem.tpl
+envsubst < /etc/letsencrypt/live/yourlifecaddie.com/chain.pem.tpl
 envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
 nginx -g 'daemon off;'
