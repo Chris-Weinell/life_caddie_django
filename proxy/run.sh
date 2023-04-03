d71f98cdb78130c4116cb6820cafd7ec51fadbea
Chris Weinell <weinellcj@live.com>
2023-04-02 21:19:48 -0400

    specify env vars in run.sh declarations

--- proxy/run.sh
+++ proxy/run.sh
@@ -6,9 +6,9 @@ set -e
 # envsubst < /etc/letsencrypt/live/yourlifecaddie.com/privkey.pem.tpl > /etc/letsencrypt/live/yourlifecaddie.com/privkey.pem
 # envsubst < /etc/letsencrypt/live/yourlifecaddie.com/cert.pem.tpl > /etc/letsencrypt/live/yourlifecaddie.com/cert.pem
 # envsubst < /etc/letsencrypt/live/yourlifecaddie.com/chain.pem.tpl > /etc/letsencrypt/live/yourlifecaddie.com/chain.pem
-envsubst -V < /etc/letsencrypt/live/yourlifecaddie.com/fullchain.pem.tpl
-envsubst -V < /etc/letsencrypt/live/yourlifecaddie.com/privkey.pem.tpl
-envsubst -V < /etc/letsencrypt/live/yourlifecaddie.com/cert.pem.tpl
-envsubst -V < /etc/letsencrypt/live/yourlifecaddie.com/chain.pem.tpl
+envsubst '$FULLCHAIN1 $FULLCHAIN2 $FULLCHAIN3' < /etc/letsencrypt/live/yourlifecaddie.com/fullchain.pem.tpl
+envsubst < /etc/letsencrypt/live/yourlifecaddie.com/privkey.pem.tpl
+envsubst < /etc/letsencrypt/live/yourlifecaddie.com/cert.pem.tpl
+envsubst < /etc/letsencrypt/live/yourlifecaddie.com/chain.pem.tpl
 envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
 nginx -g 'daemon off;'
