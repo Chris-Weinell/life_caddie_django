5fe9b1ef9151f7a5e404ce757ee37e43343eaa34
Chris Weinell <weinellcj@live.com>
2023-05-01 13:42:00 -0400

    remove all letsencrypt references in docker-compose-deploy.yml and proxy directory

--- proxy/run.sh
+++ proxy/run.sh
@@ -2,9 +2,5 @@
 
 set -e
 
-cat /etc/letsencrypt/live/yourlifecaddie.com/fullchain.pem.tpl > /etc/letsencrypt/live/yourlifecaddie.com/fullchain.pem
-cat /etc/letsencrypt/live/yourlifecaddie.com/privkey.pem.tpl > /etc/letsencrypt/live/yourlifecaddie.com/privkey.pem
-cat /etc/letsencrypt/live/yourlifecaddie.com/cert.pem.tpl > /etc/letsencrypt/live/yourlifecaddie.com/cert.pem
-cat /etc/letsencrypt/live/yourlifecaddie.com/chain.pem.tpl > /etc/letsencrypt/live/yourlifecaddie.com/chain.pem
 envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
 nginx -g 'daemon off;'
